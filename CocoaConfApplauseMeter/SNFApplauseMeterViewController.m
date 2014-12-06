5e1031a738d61d3a0fd478a907a9372dd1f48c8a
Chris Adamson <invalidname@gmail.com>
2014-12-06 09:35:29 -0500

    Corrected copyright statement to CC0.

--- CocoaConfApplauseMeter/SNFApplauseMeterViewController.m
+++ CocoaConfApplauseMeter/SNFApplauseMeterViewController.m
@@ -3,7 +3,7 @@
 //  CocoaConfApplauseMeter
 //
 //  Created by Chris Adamson on 11/24/12.
-//  Copyright (c) 2012 Subsequently & Furthermore, Inc. All rights reserved.
+//  Copyright (c) 2014 Subsequently & Furthermore, Inc. CC0 License - http://creativecommons.org/about/cc0//
 //
 
 #import "SNFApplauseMeterViewController.h"
